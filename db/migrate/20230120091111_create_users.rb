74e7917d34ddeb6ae455f8c0e2343ba04acb3439
Yash Vasundhariya <yashvasundhariya.1@gmail.com>
2023-01-20 15:49:56 +0530

    adding device successfully

---
+++ db/migrate/20230120091111_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :username
+      t.string :contact
+      t.integer :age
+      t.string :gender
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_111815) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_092425) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -21,6 +21,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_111815) do
     t.boolean "is_paid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "contact"
+    t.integer "age"
+    t.string "gender"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
